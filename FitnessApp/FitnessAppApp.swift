284ccaccf1c1fe8bd85c3190759c522921103994
eshnag <eshnag04@gmail.com>
2026-02-16 20:07:36 -0800

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Eshna Gupta on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
